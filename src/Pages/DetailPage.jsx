4121fbc9b85b1fe5ef9a86930dad70e27946f93f
Seung taek Oh <elqkddl@naver.com>
2023-08-07 14:48:47 +0900

    [Feat] DetailPage 관심사의 분리

--- src/Pages/DetailPage.jsx
+++ src/Pages/DetailPage.jsx
@@ -6,6 +6,7 @@ import ReactMarkdown from 'react-markdown';
 import styled from 'styled-components';
 import moment from 'moment';
 import { ErrorPage } from './ErrorPage';
+import { IssueDetail } from '../components/IssueDetail/IssueDetail';
 
 function DetailPage() {
   const { number } = useParams();
@@ -23,61 +24,10 @@ function DetailPage() {
   }
 
   return (
-    <Wrapper>
-      <BoxContainer>
-        <Title>title : {issue?.title}</Title>
-        <IssueNumber>#{issue?.number}</IssueNumber>
-        <Avatar src={issue?.user.avatar_url} alt="avatar" />
-        <Writer>작성자:{issue?.user.login}</Writer>
-        <Date>만든날짜 : {moment(issue?.created_at).format('YYYY-MM-DD')}</Date>
-        <Comments>코멘트 : {issue?.comments}</Comments>
-      </BoxContainer>
-      <ReactMarkdown remarkPlugins={[remarkGfm]}>{issue?.body}</ReactMarkdown>
-    </Wrapper>
+    <>
+      <IssueDetail issue={issue} />
+    </>
   );
 }
-
-// styeld-Component
-
-const Wrapper = styled.div`
-  width: 1200px;
-  margin: 100px auto;
-  /* background-color: pink; */
-
-  position: relative;
-`;
-
-const BoxContainer = styled.div`
-  border-bottom: 1px solid #ccc;
-  padding-bottom: 20px;
-`;
-
-const Title = styled.h3`
-  position: absolute;
-  top: -20px;
-  left: 110px;
-`;
-
-const IssueNumber = styled.div`
-  position: absolute;
-  left: 60px;
-`;
-
-const Writer = styled.span`
-  margin-left: 10px;
-`;
-
-const Avatar = styled.img`
-  width: 50px;
-  hegith: 50px;
-`;
-
-const Comments = styled.span`
-  margin-left: 15px;
-`;
-
-const Date = styled.span`
-  margin-left: 10px;
-`;
 
 export default DetailPage;
--- src/components/index.js
+++ src/components/index.js
@@ -2,4 +2,3 @@ export * from './Header/Header';
 export * from './IssueList/IssueList';
 export * from './IssueListItem/IssueListItem';
 export * from './IssueListAd/IssueListAd';
-export * from './IssueDetail/IssueDetail';
