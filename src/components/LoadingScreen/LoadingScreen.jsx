b8dd83829e1e7e6314ad51e13c2a4d394eede837
Seung taek Oh <elqkddl@naver.com>
2023-08-09 22:50:43 +0900

    [Fix] CSS 수정

--- src/components/LoadingScreen/LoadingScreen.jsx
+++ src/components/LoadingScreen/LoadingScreen.jsx
@@ -1,6 +1,6 @@
 import { useContext } from 'react';
 import { GithubContext } from '../../context/GithubContext';
-import { Background, LoadingText, Wrap } from './LoadingScreen.styled';
+import { Background, LoadingText } from './LoadingScreen.styled';
 import Spinner from '../../assets/spinner.gif';
 
 const LoadingScreen = () => {
@@ -10,12 +10,10 @@ const LoadingScreen = () => {
   return (
     <>
       {isLoading && (
-        <Wrap>
-          <Background>
-            <LoadingText>잠시만 기다려 주세요.</LoadingText>
-            <img src={Spinner} alt="로딩중" width="5%" />
-          </Background>
-        </Wrap>
+        <Background>
+          <LoadingText>잠시만 기다려 주세요.</LoadingText>
+          <img src={Spinner} alt="로딩중" width="5%" />
+        </Background>
       )}
     </>
   );
--- src/components/LoadingScreen/LoadingScreen.styled.jsx
+++ src/components/LoadingScreen/LoadingScreen.styled.jsx
@@ -1,15 +1,6 @@
 import styled from 'styled-components';
 
-export const Wrap = styled.div`
-  position: relative;
-`;
-
 export const Background = styled.div`
-  position: absolute;
-  width: 100vw;
-  height: 100vh;
-  top: 0;
-  left: 0;
   background: #ffffffb7;
   display: flex;
   flex-direction: column;
